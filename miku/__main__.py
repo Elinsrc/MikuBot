6be575af6540a4bb2f5e754c036c2f37ffd01e80
Elinsrc <99191833+Elinsrc@users.noreply.github.com>
2026-07-03 19:04:20 +0500

    miku: use asyncio.run

--- miku/__main__.py
+++ miku/__main__.py
@@ -63,13 +63,4 @@ async def main():
 
 
 if __name__ == "__main__":
-    # open new asyncio event loop
-    event_policy = asyncio.get_event_loop_policy()
-    event_loop = event_policy.new_event_loop()
-    asyncio.set_event_loop(event_loop)
-
-    # start the bot
-    event_loop.run_until_complete(main())
-
-    # close asyncio event loop
-    event_loop.close()
+    asyncio.run(main())
